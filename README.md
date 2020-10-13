c3aff33154716b877e7dda24d424e3f3408f50b1
vijayp23 <vijayp.github@gmail.com>
2020-10-13 23:56:00 +0530

    Delete GAE versions action

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # delete-gae-version-action
+
+This action will allow you to delete [Google App Engine](https://cloud.google.com/appengine) versions which do not receive any traffic.
+
+## Usage
+```yaml
+- name: Delete GAE versions
+  uses: vp-ework/delete-gae-version-action@master
+  with:
+    service-account: ${{ secrets.SERVICE_ACCOUNT }}
+    project-id: ${{ secrets.PROJECT_ID }}
+    service-name: ${{ secrets.SERVICE_NAME }}
+    # Optional, default is 0
+    retain-versions: 1
+```
+## Inputs
+
+**Required**
+* `service-account`: service account key which will be used for authentication
+    *  [Create a service account key](https://cloud.google.com/iam/docs/creating-managing-service-account-keys)
+    * Encode service account key as `base64` string 
+        - `cat service-account-key.json | base64` on Linux or macOS
+    * Store `base64` value in[ GitHub secret](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/creating-and-using-encrypted-secrets)
+
+* `project-id`: GCP project id in which GAE service is available
+
+* `service-name`: name of the GAE service
+
+**Optional**
+* `retain-versions`: number of versions to be retained (not deleted). Versions are ordered based on last deployed date. This option will allow to keep latest versions.
+
+## Note
+* You cannot delete a version of a service that is currently receiving traffic
+
+
---
+++ index.js
@@ -0,0 +1,62 @@
+const core = require('@actions/core');
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+async function run() {
+  try {
+    
+    const keyFile = `/tmp/${(new Date()).getTime()}.json`; 
+    const projectId = core.getInput('project-id');
+    const serviceName = core.getInput('service-name');
+    const retainVersions = core.getInput('retain-versions');
+    var isDebug = core.getInput('debug');
+
+    core.startGroup('Copy service account');
+    console.log('Copy service account');
+    const keyFileBase64 = core.getInput('service-account')
+    const keyFileContents = Buffer.from(keyFileBase64, 'base64').toString()
+    fs.writeFileSync(keyFile, keyFileContents);
+    core.endGroup();
+
+    core.startGroup('Activate service account');
+    console.log('Activate service account');
+    execSync(`gcloud auth activate-service-account --key-file ${keyFile}`, { stdio: 'inherit' });
+    core.endGroup();
+
+    core.startGroup('List of all available versions');
+    execSync(`gcloud app services list --format="table[box,title='Version List'](versions.id:label=VERSION,versions.traffic_split,versions.last_deployed_time.datetime:sort=1:label=DEPLOYED_TIME)" --flatten=versions --project=${projectId} --filter="id:${serviceName}"`, { stdio: 'inherit' });
+    core.endGroup();
+
+    core.startGroup('List versions to be deleted');
+    var versionList = JSON.parse(execSync(`gcloud app versions list --project=${projectId} --service=${serviceName} --filter=TRAFFIC_SPLIT=0 --sort-by=~"last_deployed_time" --format="json"`).toString());
+    var versions = "";
+    for(var index in versionList){
+        if ( index > retainVersions -1){
+            console.log("Version: " + versionList[index]["id"] + " DeployedOn: " + versionList[index]["last_deployed_time"]["datetime"] );
+            versions +=versionList[index]["id"]+" ";
+        }
+    }
+
+    if (versions == ""){
+      isDebug = true;
+      console.log("There are no versions available to delete..");
+    }
+
+    core.endGroup();
+
+    if (!isDebug) {
+      core.startGroup('Delete service versions');
+      execSync(`gcloud app versions delete --project=${projectId} --service=${serviceName} ${versions} --quiet`, {stdio: 'inherit'});
+      core.endGroup();
+    }
+
+    core.startGroup('Remove service account');
+    fs.unlinkSync(keyFile);
+    core.endGroup();
+  } 
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
